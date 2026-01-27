ec44038339ad3d403a75765605bb7e9388892a0a
Lucas Paduam <lucasepaduam@gmail.com>
2026-01-27 19:31:41 -0400

    Implementação da rota GET para listar todos os Pets

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import "dotenv/config";
 import express from "express";
 import { pets } from "./pets.js";
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get("/pets", (req, res) => {
+  try {
+    res.status(200).json(pets);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ mensagem: "Erro ao buscar pets", erro: error.message });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("API de Pets com Dotenv configurado! 🐾");
--- src/pets.js
+++ src/pets.js
@@ -1,1 +1,9 @@
-export const pets = [];
+export const pets = [
+  {
+    id: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed",
+    nome: "Irlandes",
+    raca: "Golden Irlandes",
+    idade: 6,
+    nomeTutor: "Lucas",
+  },
+];
