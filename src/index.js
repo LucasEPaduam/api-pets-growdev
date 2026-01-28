8249166affc5ae6a7f8a7693ba175107fece03b7
Lucas Paduam <lucasepaduam@gmail.com>
2026-01-28 15:29:58 -0400

    Implementada a rota DELETE

--- src/index.js
+++ src/index.js
@@ -12,6 +12,14 @@ app.use(express.json());
 
 app.get("/pets", (req, res) => {
   try {
+    if (pets.length === 0) {
+      return res.status(200).send({
+        ok: true,
+        mensagem: "Nenhum pet cadastrado no momento",
+        pets: [],
+      });
+    }
+
     res.status(200).send({
       ok: true,
       mensagem: "Pets encontrados com sucesso",
@@ -100,13 +108,39 @@ app.put("/pets/:id", [validarPet], (req, res) => {
       pet: pets[petIndex],
     });
   } catch (error) {
-    res
-      .status(500)
-      .send({
-        ok: false,
-        mensagem: "Erro ao atualizar pet",
-        erro: error.message,
-      });
+    res.status(500).send({
+      ok: false,
+      mensagem: "Erro ao atualizar pet",
+      erro: error.message,
+    });
+  }
+});
+
+app.delete("/pets/:id", (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const petIndex = pets.findIndex((p) => p.id === id);
+
+    if (petIndex === -1) {
+      return res
+        .status(404)
+        .send({ ok: false, mensagem: "Pet não encontrado para exclusão" });
+    }
+
+    const petsRemovidos = pets.splice(petIndex, 1);
+
+    res.status(200).send({
+      ok: true,
+      mensagem: `Pet foi removido com sucesso.`,
+      petRemovido: petsRemovidos[0],
+    });
+  } catch (error) {
+    res.status(500).send({
+      ok: false,
+      mensagem: "Erro ao excluir pet",
+      erro: error.message,
+    });
   }
 });
 
